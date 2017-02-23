d7c8a1f03c31b7862b0ed5b4956c98cd97c32936
JP Strydom <u13010736@tusk.co.za>
2017-02-23 21:08:00 +0200

    Add endpoint array

--- qualify/IO.py
+++ qualify/IO.py
@@ -14,17 +14,26 @@ def ReadFile(fileName):
     videoCount, endpointCount, requestCount, cachesCount, capacity = line
     videoSizes = np.array(f.readline().split(' '), dtype=int)
 
-    endpoints = []
+    endpoints = np.zeros((2, endpointCount), dtype=object )
+
     for e in xrange(endpointCount):
-        array = np.array(f.readline().split(' '), dtype=int)
-        latency, connected = array
+        latency, connected = np.array(f.readline().split(' '), dtype=int)
+        endpoints[0, e] = latency
+        tempArray = np.zeros((2, connected))
         for c in xrange(connected):
-            np.array(f.readline().split(' '), dtype=int)
+            cacheID, cacheLatency = np.array(f.readline().split(' '), dtype=int)
+            tempArray[0, c] = cacheID
+            tempArray[1, c] = cacheLatency
+
+        endpoints[1, e] = np.array(tempArray)
+        #tempArray
+        print endpoints
+
 
     requests = [ # video, endpoint, count
         np.array(f.readline().split(' '), dtype=int).tolist() for r in xrange(requestCount)
     ]
 
-    requests = requests[requests[:,2].argsort()]
+    #requests = requests[requests[:,2].argsort()]
 
     return videoSizes, endpoints, requests, cachesCount, capacity
