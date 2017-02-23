979f3507fe526a0ffb32cca7fc7e5cbb96df4c05
Renette Ros <renette.ros@entelect.co.za>
2017-02-23 22:28:30 +0200

    Added output to main

--- qualify/Main.py
+++ qualify/Main.py
@@ -9,30 +9,30 @@ import IO as IO
 from operator import itemgetter
 
 import numpy as np
+import Scoring as Sc
 
-test_in = "test.in"
+test_in = "kittens.in"
 videoSizes, endpoints, requests, cachesCount, capacity = IO.ReadFile(test_in)
 
 requests = sorted(requests, key=itemgetter(2), reverse=True)
 
+
 def getMaxCapacity(l):
     return max(zip(*l)[0])
 
+
 caches = [[100, []] for _ in xrange(cachesCount)]
-
-
-
 
 while True:
     if len(requests) <= 0:
-        break # Storage space left
+        break  # Storage space left
     req = requests.pop(0)
     videoId = req[0]
     videoSize = videoSizes[videoId]
 
     endpId = req[1]
     endp = endpoints[endpId]
-    serverLatencies = sorted(endp[1], key=itemgetter(1)) # cacheId, cacheLatency
+    serverLatencies = sorted(endp[1], key=itemgetter(1))  # cacheId, cacheLatency
     if len(serverLatencies) == 0:
         continue
     serverDetails = [caches[s[0]] for s in serverLatencies]
@@ -56,21 +56,7 @@ while True:
             cacheObj[1].append(videoId)
             break
 
-
+Sc.output(caches)
 print caches
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 print "Done"
